ccb9d00f74a8bf7bcfacde81200f5352eef82860
Fizza Batool <batoolfizz704@gmail.com>
2025-05-11 22:36:54 +0500

    Add files via upload

---
+++ variables_practice.py
@@ -0,0 +1,28 @@
+
+###Declare Variables
+
+first_name = "Fizza"
+last_name = "Batool"
+course_name = "Python Programming"
+
+print(first_name)
+print(last_name)
+print(course_name)
+
+
+##Invalid Variables
+
+#9first_name = "Fizza"             #Cannot start with a number
+#last//name = "Batool"              #Cannot use special characters(!,@,$)
+##course name ="Python Programming" #Cannot use spaces 
+#print = 100                         #Cannot use reserved keywords(print, if ,for)
+
+
+##Apply Best Practices
+
+my_first_name = "Fizza"
+my_last_name = "Batool"
+my_total_marks = "100"
+print(my_first_name)
+print(my_last_name)
+print(my_total_marks)
